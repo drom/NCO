6b1e8235eedf6d26cd3bd5bc6236d24bc6a74db6
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2018-03-16 23:51:38 -0700

    lookup table generator

---
+++ lut.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+'use strict';
+
+const range = length => Array.apply(null, Array(length)).map((e, i) => i);
+
+// calculate complex exponent look-up table
+
+const points = 16;
+
+// pi/4 part of data
+const step = Math.PI / 4 / points;
+
+// initial gain need to be adjusted depend on number CORDIC steps
+const gain = 1 << 14;
+
+const digits = 5;
+
+const val2hex = val =>
+    ('000000000' + Math.round(gain * val).toString(16)).slice(-digits);
+
+const res = range(points)
+    .map(i => step * i)
+    .map(phi => ({
+        re: (phi === 0) ? Math.sin(Math.PI / 4) : Math.sin(phi),
+        im: Math.cos(phi)
+    }))
+    .map(cplx => ({
+        re: val2hex(cplx.re),
+        im: val2hex(cplx.im),
+    }));
+
+console.log(res);

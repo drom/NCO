802fa816928e443cdedd6a223b11a34bcdfba8e7
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2018-03-17 00:08:08 -0700

    parsed CLI arguments

--- lut.js
+++ lut.js
@@ -1,19 +1,21 @@
 #!/usr/bin/env node
 'use strict';
 
+const argv = process.argv;
+
 const range = length => Array.apply(null, Array(length)).map((e, i) => i);
 
 // calculate complex exponent look-up table
+const points = parseInt(argv[2]) || 16;
 
-const points = 16;
+// number of HEX digits in the output sin/cos value
+const digits = parseInt(argv[3]) || 4;
+
+// initial gain need to be adjusted depend on number CORDIC steps
+const gain = Number(argv[4]) || (1 << 14);
 
 // pi/4 part of data
 const step = Math.PI / 4 / points;
-
-// initial gain need to be adjusted depend on number CORDIC steps
-const gain = 1 << 14;
-
-const digits = 5;
 
 const val2hex = val =>
     ('000000000' + Math.round(gain * val).toString(16)).slice(-digits);

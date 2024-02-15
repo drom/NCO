b51438a22bb3f57c56a9d209c55006643b2372cc
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2024-02-15 15:52:40 -0800

    fix commander arguments

--- bin/nco.js
+++ bin/nco.js
@@ -11,12 +11,12 @@ const ncoVerilog = require('../lib/nco-verilog.js');
 
 const main = async () => {
   program
-    .option('--dataWidth <number>', 'I/Q LUT data width [bit] : 2 * ', parseInt, 16)
-    .option('--addrWidth <number>', 'LUT address width [bit]', parseInt, 4)
-    .option('--betaWidth <number>', 'Beta angle width [bit]', parseInt, 16)
-    .option('--nCordics <number>',  'number of CORDIC stages', parseInt, 9)
-    .option('--corrector <number>', 'CORDIC step correction', parseInt, 0)
-    .option('--scale <number>',     'scale correction', parseFloat, 0.999838)
+    .option('--dataWidth <number>', 'I/Q LUT data width [bit] : 2 * ', n => parseInt(n), 16)
+    .option('--addrWidth <number>', 'LUT address width [bit]', n => parseInt(n), 4)
+    .option('--betaWidth <number>', 'Beta angle width [bit]', n => parseInt(n), 16)
+    .option('--nCordics <number>',  'number of CORDIC stages', n => parseInt(n), 9)
+    .option('--corrector <number>', 'CORDIC step correction', n => parseInt(n), 0)
+    .option('--scale <number>',     'scale correction', n => parseFloat(n), 0.999838)
     .option('--top <name>',         'Top level name', 'nco')
     .parse(process.argv);
 

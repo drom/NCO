42357c0e08c8e74e6dd6e0c58db97ab0f68fc200
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2024-02-15 15:10:11 -0800

    new year, new eslint rules

--- src/app.js
+++ src/app.js
@@ -18,60 +18,60 @@ const Verilog = reGenVerilog(React)({});
 
 class App extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {data: props.data};
-        this.updateState = this.updateState.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {data: props.data};
+    this.updateState = this.updateState.bind(this);
 
-        this.Form = genForm({
-            schema: {
-                type: 'object',
-                properties: {
-                    dataWidth: {type: 'integer', minimum: 4, maximum: 30, title: 'I/Q LUT data width [bit] : 2 * '},
-                    addrWidth: {type: 'integer', minimum: 0, maximum: 18, title: 'LUT address width [bit] : '},
-                    betaWidth: {type: 'integer', minimum: 0, maximum: 32, title: 'Beta angle width [bit] : '},
-                    nCordics:  {type: 'integer', minimum: 0, maximum: 12, title: 'number of CORDIC stages : '},
-                    corrector: {type: 'integer', minimum: -2, maximum: 2,  title: 'CORDIC step correction : '},
-                    scale:     {type: 'number',  minimum: 0, maximum: 2,  title: 'scale correction : '},
-                    top:       {type: 'string',                           title: 'top level name : '}
-                }
-            },
-            path: [],
-            updateState: this.updateState
-        });
-    }
+    this.Form = genForm({
+      schema: {
+        type: 'object',
+        properties: {
+          dataWidth: {type: 'integer', minimum: 4, maximum: 30, title: 'I/Q LUT data width [bit] : 2 * '},
+          addrWidth: {type: 'integer', minimum: 0, maximum: 18, title: 'LUT address width [bit] : '},
+          betaWidth: {type: 'integer', minimum: 0, maximum: 32, title: 'Beta angle width [bit] : '},
+          nCordics:  {type: 'integer', minimum: 0, maximum: 12, title: 'number of CORDIC stages : '},
+          corrector: {type: 'integer', minimum: -2, maximum: 2,  title: 'CORDIC step correction : '},
+          scale:     {type: 'number',  minimum: 0, maximum: 2,  title: 'scale correction : '},
+          top:       {type: 'string',                           title: 'top level name : '}
+        }
+      },
+      path: [],
+      updateState: this.updateState
+    });
+  }
 
-    updateState (spec) {
-        this.setState(function (state) {
-            return update(state, spec);
-        });
-    }
+  updateState (spec) {
+    this.setState(function (state) {
+      return update(state, spec);
+    });
+  }
 
-    render () {
-        const config = this.state.data;
-        const res = testbench(config);
-        return (
-            $('span', {},
-                $(this.Form, {data: config}),
-                $(Verilog, config),
-                $(LogPlot, {data: res.evms}),
-                $(Chart, {data: res.contours})
-            )
-        );
-    }
+  render () {
+    const config = this.state.data;
+    const res = testbench(config);
+    return (
+      $('span', {},
+        $(this.Form, {data: config}),
+        $(Verilog, config),
+        $(LogPlot, {data: res.evms}),
+        $(Chart, {data: res.contours})
+      )
+    );
+  }
 }
 
 ReactDOM.render(
-    $(App, {data: {
-        dataWidth: 16,
-        addrWidth: 4,
-        betaWidth: 16,
-        nCordics: 9,
-        corrector: 0,
-        scale: 0.999838,
-        top: 'nco'
-    }}),
-    document.getElementById('root')
+  $(App, {data: {
+    dataWidth: 16,
+    addrWidth: 4,
+    betaWidth: 16,
+    nCordics: 9,
+    corrector: 0,
+    scale: 0.999838,
+    top: 'nco'
+  }}),
+  document.getElementById('root')
 );
 
 /* eslint-env browser */
--- test/model.js
+++ test/model.js
@@ -47,63 +47,63 @@ const hsl = idx => 'hsl(' + (idx * 77) % 360 + ', 100%, 40%)';
 // };
 
 const hullChart = (props) => {
-    const w = 1024;
-    const h = 1024;
-    return ['svg', {
-        xmlns: 'http://www.w3.org/2000/svg',
-        width: w + 1,
-        height: h + 1,
-        viewBox: [0, 0, w + 1, h + 1].join(' ')
-    },
-    ['g', {transform: 'translate(.5, .5)'}].concat([
-        ['rect', {
-            x: 0, y: 0, width: w, height: h,
-            fill: 'none', stroke: 'black'
-        }],
-        ['g', {'stroke-dasharray': '2,6'}].concat(range(31, 0).map(i => ['line', {
-            stroke: '#aaa',
-            x1: 0, x2: w,
-            y1: 32 * i, y2: 32 * i
-        }])),
-        ['line', {stroke: '#aaa', x1: 0, x2: w, y1: 32 * 16, y2: 32 * 16}],
+  const w = 1024;
+  const h = 1024;
+  return ['svg', {
+    xmlns: 'http://www.w3.org/2000/svg',
+    width: w + 1,
+    height: h + 1,
+    viewBox: [0, 0, w + 1, h + 1].join(' ')
+  },
+  ['g', {transform: 'translate(.5, .5)'}].concat([
+    ['rect', {
+      x: 0, y: 0, width: w, height: h,
+      fill: 'none', stroke: 'black'
+    }],
+    ['g', {'stroke-dasharray': '2,6'}].concat(range(31, 0).map(i => ['line', {
+      stroke: '#aaa',
+      x1: 0, x2: w,
+      y1: 32 * i, y2: 32 * i
+    }])),
+    ['line', {stroke: '#aaa', x1: 0, x2: w, y1: 32 * 16, y2: 32 * 16}],
 
-        ['g', {'stroke-dasharray': '2,6'}].concat(range(32).map(i => ['line', {
-            stroke: '#aaa',
-            x1: 32 * i, x2: 32 * i,
-            y1: 0, y2: h
-        }])),
-        ['line', {stroke: '#aaa', x1: 32 * 16, x2: 32 * 16, y1: 0, y2: h}],
+    ['g', {'stroke-dasharray': '2,6'}].concat(range(32).map(i => ['line', {
+      stroke: '#aaa',
+      x1: 32 * i, x2: 32 * i,
+      y1: 0, y2: h
+    }])),
+    ['line', {stroke: '#aaa', x1: 32 * 16, x2: 32 * 16, y1: 0, y2: h}],
 
-        ['line', {stroke: '#aaa', x1: 0, x2: w, y1: 0, y2: 32 * 32}],
-        ['line', {stroke: '#aaa', x1: 0, x2: w, y1: 32 * 32, y2: 0}],
+    ['line', {stroke: '#aaa', x1: 0, x2: w, y1: 0, y2: 32 * 32}],
+    ['line', {stroke: '#aaa', x1: 0, x2: w, y1: 32 * 32, y2: 0}],
 
-        ['g', {transform: 'translate(512,512)'}].concat(props.data.map((contours, i) =>
-            ['g', {}].concat(contours.map((contour) =>
-                ['path', {fill: 'none', stroke: hsl(i), d: 'M' + contour.map(e => e[0] + ' ' + e[1]).join('L') + 'Z'}]
-            ))
-        ))
-        // ['g', {}].concat(props.data.map((e, i) => ['rect', {
-        //     fill: 'none', stroke: hsl(i),
-        //     x: e.re.min + w / 2,
-        //     width: e.re.max - e.re.min,
-        //     y: e.im.min + h / 2,
-        //     height: e.im.max - e.im.min
-        // }]))
-    ])
-    ];
+    ['g', {transform: 'translate(512,512)'}].concat(props.data.map((contours, i) =>
+      ['g', {}].concat(contours.map((contour) =>
+        ['path', {fill: 'none', stroke: hsl(i), d: 'M' + contour.map(e => e[0] + ' ' + e[1]).join('L') + 'Z'}]
+      ))
+    ))
+    // ['g', {}].concat(props.data.map((e, i) => ['rect', {
+    //     fill: 'none', stroke: hsl(i),
+    //     x: e.re.min + w / 2,
+    //     width: e.re.max - e.re.min,
+    //     y: e.im.min + h / 2,
+    //     height: e.im.max - e.im.min
+    // }]))
+  ])
+  ];
 };
 
 const exp = phase => {
-    const phi = Math.PI * (phase / ((1 << 31) >>> 0));
-    return {
-        re: Math.cos(phi),
-        im: Math.sin(phi)
-    };
+  const phi = Math.PI * (phase / ((1 << 31) >>> 0));
+  return {
+    re: Math.cos(phi),
+    im: Math.sin(phi)
+  };
 };
 
 const errVectorRot = (ref, res) => ({
-    re:  (res.re * ref.re + res.im * ref.im) - 1,
-    im: -res.im * ref.re + res.re * ref.im
+  re:  (res.re * ref.re + res.im * ref.im) - 1,
+  im: -res.im * ref.re + res.re * ref.im
 });
 
 // const errVector = (ref, res) => ({
@@ -120,11 +120,11 @@ const errVectorRot = (ref, res) => ({
 const dbrange = 512;
 
 const scaler = val => {
-    const tmp = Math.log2(val);
-    if (tmp === -Infinity) {
-        return -dbrange;
-    }
-    return 16 * tmp;
+  const tmp = Math.log2(val);
+  if (tmp === -Infinity) {
+    return -dbrange;
+  }
+  return 16 * tmp;
 };
 
 const scaler1 = val => (val > 0) ? (dbrange + scaler(val)) : (-dbrange - scaler(-val));
@@ -149,59 +149,59 @@ const scaler1 = val => (val > 0) ? (dbrange + scaler(val)) : (-dbrange - scaler(
 // };
 
 const hullErr = arr => {
-    // const cont = concaveHull()(arr.map(e => [e.re, e.im]));
+  // const cont = concaveHull()(arr.map(e => [e.re, e.im]));
 
-    // const cont = concaveHull().distance(1)(arr.map(e => [e.re, e.im]));
-    // const cont1 = cont.map(contour => contour.map(e => [scaler1(e[0]), scaler1(e[1])]));
+  // const cont = concaveHull().distance(1)(arr.map(e => [e.re, e.im]));
+  // const cont1 = cont.map(contour => contour.map(e => [scaler1(e[0]), scaler1(e[1])]));
 
-    const cont = d3Polygon.polygonHull(arr.map(e => [e.re, e.im]));
-    const cont1 = [cont.map(e => [scaler1(e[0]), scaler1(e[1])])];
+  const cont = d3Polygon.polygonHull(arr.map(e => [e.re, e.im]));
+  const cont1 = [cont.map(e => [scaler1(e[0]), scaler1(e[1])])];
 
-    // console.log(cont1);
+  // console.log(cont1);
 
 
-    return cont1;
+  return cont1;
 };
 
 const randomPhase = () => (Math.random() * (1 << 31)) >>> 0;
 
 describe('Model', () => {
-    it('lut', () => {
-        let ret = [];
-        [
-            [1,  0], // [1,  1], [1,  2], [1,  3], [1,  4], [1,  5]
-            [2,  0], // [2,  1], [2,  2], [2,  3], // [2,  4], [2,  5]
-            [3,  0], // [3,  1], [3,  2], [3,  3], [3,  4], [3,  5]
-            [4,  0], [4,  1], [4,  2], [4,  3], [4,  4], [4,  5]
-            // [5,  0], [5,  1], [5,  2], [5,  3], [5,  4], [5,  5]
-            // [6,  0], [6,  1], [6,  2], [6,  3], [6,  4], [6,  5]
-            // [7,  0], [7,  1], [7,  2], [7,  3], [7,  4], [7,  5]
-            // [8,  0], [8,  1], [8,  2], [8,  3], [8,  4], [8,  5]
-            // [9,  0], // [9,  1], [9,  2], [9,  3], [9,  4], [9,  5]
-            // [10, 0], // [10, 1], [10, 2], [10, 3], [10, 4], [10, 5]
-            // [11, 0], [11, 1], // [11, 2], [11, 3], [11, 4], [11, 5]
-            // [12, 0], // [12, 1], [12, 2], [12, 3], [12, 4], [12, 5]
-            // [13, 0], // [13, 1], [13, 2], [13, 3], [13, 4], [13, 5]
-        ].map(props => {
-            const addrSize = props[0];
-            const nCordics = props[1];
-            const model = genModel(addrSize, 12, nCordics, 2);
-            const errors = Array(5000).fill(0).map(() => {
-                const phase = randomPhase();
-                // const phase = (i << (32 - 3 - 2) >>> 0);
-                const res = model(phase);
-                const ref = exp(phase);
-                // return errMag(ref, res);
-                return errVectorRot(ref, res);
-            });
-            // ret.push(boxErr(errors));
-            ret.push(hullErr(errors));
-            // done();
-        });
-        // console.log(ret);
-        // console.log(boxChart({data: ret}));
-        // fs.outputFile('./box-chart.svg', onml.s(boxChart({data: ret})), () => {});
-        fs.outputFile('./box-chart.svg', onml.s(hullChart({data: ret})), () => {});
+  it('lut', () => {
+    let ret = [];
+    [
+      [1,  0], // [1,  1], [1,  2], [1,  3], [1,  4], [1,  5]
+      [2,  0], // [2,  1], [2,  2], [2,  3], // [2,  4], [2,  5]
+      [3,  0], // [3,  1], [3,  2], [3,  3], [3,  4], [3,  5]
+      [4,  0], [4,  1], [4,  2], [4,  3], [4,  4], [4,  5]
+      // [5,  0], [5,  1], [5,  2], [5,  3], [5,  4], [5,  5]
+      // [6,  0], [6,  1], [6,  2], [6,  3], [6,  4], [6,  5]
+      // [7,  0], [7,  1], [7,  2], [7,  3], [7,  4], [7,  5]
+      // [8,  0], [8,  1], [8,  2], [8,  3], [8,  4], [8,  5]
+      // [9,  0], // [9,  1], [9,  2], [9,  3], [9,  4], [9,  5]
+      // [10, 0], // [10, 1], [10, 2], [10, 3], [10, 4], [10, 5]
+      // [11, 0], [11, 1], // [11, 2], [11, 3], [11, 4], [11, 5]
+      // [12, 0], // [12, 1], [12, 2], [12, 3], [12, 4], [12, 5]
+      // [13, 0], // [13, 1], [13, 2], [13, 3], [13, 4], [13, 5]
+    ].map(props => {
+      const addrSize = props[0];
+      const nCordics = props[1];
+      const model = genModel(addrSize, 12, nCordics, 2);
+      const errors = Array(5000).fill(0).map(() => {
+        const phase = randomPhase();
+        // const phase = (i << (32 - 3 - 2) >>> 0);
+        const res = model(phase);
+        const ref = exp(phase);
+        // return errMag(ref, res);
+        return errVectorRot(ref, res);
+      });
+      // ret.push(boxErr(errors));
+      ret.push(hullErr(errors));
+      // done();
     });
+    // console.log(ret);
+    // console.log(boxChart({data: ret}));
+    // fs.outputFile('./box-chart.svg', onml.s(boxChart({data: ret})), () => {});
+    fs.outputFile('./box-chart.svg', onml.s(hullChart({data: ret})), () => {});
+  });
 });
 /* eslint-env mocha */
